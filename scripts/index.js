68f4d3b286f2ab3cb514357574073f9b79ba0bdd
Pavel Zakharov <trade93@gmail.com>
2022-08-16 23:46:32 +0300

    feature: script and readme

--- scripts/index.js
+++ scripts/index.js
@@ -8,3 +8,20 @@ const togglePopup = function() {
 
 editButton.addEventListener('click', togglePopup);
 closeButton.addEventListener('click', togglePopup);
+
+let formElement = document.querySelector('.popup__container');
+let nameInput = formElement.querySelector('.popup__name');
+let jobInput = formElement.querySelector('.popup__job');
+let nameInfo = document.querySelector('.profile-info__name');
+let jobInfo = document.querySelector('.profile-info__job');
+
+nameInput.value = nameInfo.textContent;
+jobInput.value = jobInfo.textContent;
+
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+  nameInfo.textContent = nameInput.value;
+  jobInfo.textContent = jobInput.value;
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
